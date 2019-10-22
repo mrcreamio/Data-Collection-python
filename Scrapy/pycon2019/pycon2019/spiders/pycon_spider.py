8036025095b91c59bfcdbd343db7208037316d43
Ahmed Waheed <creamio@outlook.com>
2019-10-22 13:47:33 +0500

    d

---
+++ Scrapy/pycon2019/pycon2019/spiders/pycon_spider.py
@@ -0,0 +1,22 @@
+from scrapy import Spider, Request
+
+class PyconSpider(Spider):
+    name = "pycon"
+    
+    def __init__(self):
+        self.url = 'http://pycon.pk'
+    
+    def start_requests(self):
+        yield Request(url=self.url, callback=self.extract_speakerslink)
+
+    def extract_speakerslink(self, response):
+        speakers_link = response.css("li[id='speakers-2019'] a::attr(href)").extract_first()
+        url = "{0}{1}".format(self.url, speakers_link)
+        yield Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        speakers_info = response.css("div[class='member-desc']")
+        from pandas import DataFrame
+        l = [[info.css("h3::text").extract_first(),info.css("h5::text").extract_first(),info.css("h6::text").extract_first()] for info in speakers_info]
+        df = DataFrame(l, columns=["Name", "Designation", "Company"])
+        df.to_csv("Speakers(Pycon2019).csv", index=False, sep=",")
